2cde6fa99a674c384c8b24af25b1153d07b715ea
AbcAjerisAbc <hasinaajeris@gmail.com>
2026-07-27 14:23:01 +0300

    environnement-3

---
+++ src/main/java/com/voronkely/VoronkelyApplication.java
@@ -0,0 +1,13 @@
+package com.voronkely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoronkelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoronkelyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/voronkely/view/ViewController.java
@@ -0,0 +1,13 @@
+package com.voronkely.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/test")
+    public String index() {
+        return "index";
+    }
+}
