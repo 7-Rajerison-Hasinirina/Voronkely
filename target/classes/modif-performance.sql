d05bc465e2339dfa05c1a488680ab30089ba9a3f
AbcAjerisAbc <hasinaajeris@gmail.com>
2026-07-27 14:02:54 +0300

    environnement-2

--- target/classes/modif-performance.sql
+++
@@ -1,6 +0,0 @@
--- Mettre à jour les lignes avec prixUnitaireFacture = NULL
-UPDATE "ligneCommande" l
-SET "prixUnitaireFacture" = p."prixBase"
-FROM "produit" p
-WHERE l."idProduit" = p."idProduit"
-AND (l."prixUnitaireFacture" IS NULL OR l."prixUnitaireFacture" = 0);
